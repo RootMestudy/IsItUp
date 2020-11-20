1b859932caa1ce927428862f2358a4773ac194ce
Florian Kamps <contact@floriankamps.fr>
2020-11-20 16:30:06 +0100

    Improve ping command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
     if (!msg.content.startsWith(config.prefix) || msg.author.bot) return;
 
 	const args = msg.content.slice(config.prefix.length).trim().split(/ +/);
@@ -14,9 +14,13 @@ client.on('message', msg => {
 
   // Simple ping command  
   if (command === 'ping') {
-    msg.reply('pong');
+    msg.channel.send("Pinging...").then(mes => {
+        let ping = Math.round(client.ws.ping)
+        mes.edit(`Latency is ${ping}ms`);
+    })
   }
 
+  
 
 });
 
