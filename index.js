7ddd3321ac98c705237c34b9f84707bbd01a3fc1
Florian Kamps <contact@floriankamps.fr>
2020-11-20 22:46:15 +0100

    Transform ping into a dynamic embed

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 
+function ping_embed(client){
+    let ping = Math.round(client.ws.ping);
+    let pingEmbed = new Discord.MessageEmbed()
+        .addFields(
+            { name: 'Bot latency', value: `${ping}ms` }
+        )
+    return (pingEmbed);
+}
+
+function edit_ping_embed(msg, client) {
+    let edit_embed = ping_embed(client);
+    msg.edit(edit_embed);
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -12,16 +26,14 @@ client.on('message', async msg => {
 	const args = msg.content.slice(config.prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-  // Simple ping command  
   if (command === 'ping') {
     msg.channel.send("Pinging...").then(mes => {
-        let ping = Math.round(client.ws.ping)
-        mes.edit(`Latency is ${ping}ms`);
+        mes.delete({ timeout: 1000 });
+        msg.channel.send(ping_embed(client)).then(mes => {
+            setInterval(() => edit_ping_embed(mes, client), 1000);
+        })
     })
   }
-
-  
-
 });
 
 client.login(config.token);
